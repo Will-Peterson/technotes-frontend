5f16e6583f46cdd8cb874a101477735a23f307a2
Will-Peterson <william.from.ca@gmail.com>
2022-11-20 03:07:26 -0800

    redux and rtk query setup

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import {useGetNotesQuery} from './notesApiSlice'
+import Note from './Note'
 
 const NotesList = () => {
-  return (
-    <h1>NotesList</h1>
-  )
+  const {
+    data: notes,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetNotesQuery()
+
+  let content
+
+  if (isLoading) content = <p>Loading...</p>
+  
+  if (isError) {
+    content = <p className='errmsg'>{error?.data?.message}</p>
+  }
+
+  if (isSuccess) {
+    const {ids} = notes
+    
+    const tableContent = ids?.length
+      ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
+      : null
+
+    content = (
+      <table className='table table--notes'>
+        <thead className='table__thead'>
+          <tr>
+            <th scope='col' className='table__th note_status'>Username</th>
+            <th scope='col' className='table__th note_created'>Created</th>
+            <th scope='col' className='table__th note_updated'>Updated</th>
+            <th scope='col' className='table__th note_title'>Title</th>
+            <th scope='col' className='table__th note_username'>Owner</th>
+            <th scope='col' className='table__th note_edit'>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+        </tbody>
+      </table>
+    )
+  }
+
+  return content
+
 }
 
 export default NotesList
